cd530d48b1ee14256d7ab5f4cf65d4b172464c5b
lepfsd-strapp <lperez@strappinc.net>
2019-03-06 00:12:37 -0400

    get note

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ console.log('starting app');
 const fs = require('fs');
 const os = require('os');
 const _ = require('lodash');
+const yargs = require('yargs');
 const notes = require('./notes.js'); 
+const argv = yargs.argv;
+
 /*let user = os.userInfo();
 
 fs.appendFile('greetings.txt', `hello ${user.username}! You are ${notes.age}.`, (err) => {
@@ -10,11 +13,27 @@ fs.appendFile('greetings.txt', `hello ${user.username}! You are ${notes.age}.`, 
 	console.log('the "hello world" was append to file');
 }); */
 
-var res = notes.addNote();
+//var res = notes.addNote();
 //console.log('Result:', notes.add(3, 2)); 
 
 //console.log(_.isString(true));
 //console.log(_.isString("Luis"));
 
-var filteredArray = _.uniq(['Luis', 1, 'Luis', 1, 2, 3, 4]);
-console.log(filteredArray);
+//var filteredArray = _.uniq(['Luis', 1, 'Luis', 1, 2, 3, 4, 2]);
+
+var command = process.argv[2];
+console.log('Command: ', command);
+console.log('Process', process.argv);
+console.log('Yargs', argv);
+
+if(command === 'add') {
+	console.log('Adding new note');
+	notes.addNote(argv.title, argv.body);
+} else if(command === 'list') {
+	console.log('List all notes');
+	notes.getAll();
+} else if(command === 'read'){
+	notes.getNote(argv.title);
+} else {
+	console.log('Command not recognized');
+}
--- notes.js
+++ notes.js
@@ -1,9 +1,27 @@
 console.log('starting notes');
 
-module.exports.addNote = () => {
+/*module.exports.addNote = () => {
 	return 'new note';
 };
 
 module.exports.add = (a, b) => {
 	return a + b;
-}
+} */
+
+var addNote = (title, body) => {
+	console.log('Adding note', title, body);
+};
+
+var getAll = () => {
+	console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+	console.log('Getting note', title);
+};
+
+module.exports = {
+	addNote,
+	getAll,
+	getNote
+};
